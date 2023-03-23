bb4dd07877d34c29ea2b192b36513bf3c29ed300
Mike Salemi <mike.salemi95@gmail.com>
2023-03-22 22:47:52 -0400

    Sockets
    Switching from 2 FIFOs to unix socket

--- Labs/Lab6/main_fifo_server.c
+++ Labs/Lab6/main_fifo_server.c
@@ -1,55 +1,52 @@
-#include <errno.h>
-#include <fcntl.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/socket.h>
+
+#include <>
 
 typedef struct {
     int nbytes;
     char data[1024];
 } transfer;
+int newfd;
 
 int main(int argc, char const *argv[]) {
-    int filenum, fifo_desc, charcount, fifonum;
-    char filename[400], buffer[1024];
-    char *fifo_toclient = "FIFO_TO_CLIENT";
-    transfer send;
-    transfer get;
-    int pid = getpid();
+    int socket_desc;
+    struct sockaddr_in srv;
+    struct sockaddr_in cli;
+    int cli_len = sizeof(cli);
+    int done = 0;
 
-    if (!(filenum = open(filename, O_CREAT | O_RDONLY, 777))) {
-        printf("File could not be read or does not exist.\n");
-        return 1;
+    signal(SIGPIPE, catcher);
+
+    if ((socket_desc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket call failed");
+        exit(1);
+    }
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons(51614);
+    srv.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(socket_desc, (struct sockaddr *)&srv), sizeof(srv) < 0) {
+        perror("bind call failed");
+        exit(1);
+    }
+    if (listen(socket_desc,1)<0) {
+        perror("listen call failed");
+        exit(1);
     }
 
-    fifo_desc = mkfifo(fifo_toclient, 0777);
-
-    if (fifo_desc == -1) {
-        if (errno != EEXIST) {
-            printf("Could not create FIFO!\n");
-            return 1;
+    while (!done) {
+        newfd = accept(socket_desc, (struct sockaddr *) &cli, &cli_len);
+        if (newfd < 0)
+        {
+            perror("Accept call failed");
+            done = 1;
         }
-        printf("FIFO already exists\n");
+        
     }
-
-    // get filename
-    open("FIFO_TO_SERVER", O_RDONLY);
-    
-
-    printf("Writing to FIFO\n");
-    charcount = lseek(filenum, 0, SEEK_CUR);
-    fifonum = open(fifo_toclient, O_WRONLY);
-    while (read(filenum, buffer, 1024)) {
-        write(fifonum, buffer, 1024);
-        lseek(filenum, 1024, SEEK_CUR);
-        printf("%s\n", buffer);
-    }
-    printf("Done writing to FIFO\n");
-
-    remove(fifo_toserver);
 
     return 0;
 }
