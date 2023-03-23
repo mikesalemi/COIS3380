c74be247989a44ea3c4927eb4e631d5c744b9a73
Mike Salemi <mike.salemi95@gmail.com>
2023-03-22 22:21:14 -0400

    Lab5

--- Labs/Lab5/main_signalProcessing.c
+++ Labs/Lab5/main_signalProcessing.c
@@ -1,4 +1,16 @@
 // main_signalProcessing.c
+// Michael Salemi
+// 0751614
+// 03/18/2022
+// Description: 
+//      This program changes the action of the "^C" and "^\" Unix signals
+//      to carry out different functions. "^C" increments a counter each 
+//      time it is pressed for a maximum of 5 times and then reverts back
+//      to it's original action and exits the program if given again. 
+//      "^\" is changed to creating a fork and passing the custom SIGUSR1
+//      signal to its parent where it will then terminate the program. This
+//      is done by setting the global variable "done" to 1 which is used as 
+//      flag for the program while loop.
 
 #include <signal.h>
 #include <stdio.h>
@@ -33,7 +45,14 @@ int main(int argc, char const *argv[]) {
 
     return 0;
 }
-
+// void signalOptions(int signo)
+// Parameters: 
+//      int signo:  this parameter catches the signal that is given while in the main function
+// Description:
+//      This function provides an alternative action to the SIGINT, SIGQUIT, and SIGUSR1 actions
+//      that are caught while the main function pauses and waits. After a set number of SIGINT
+//      uses, the program terminates using the default SIGINT action. When SIGQUIT is given,
+//      the program will also terminate. 
 void signalOptions(int signo) {
     if (signo == SIGINT) {                               // user typed ^c
         printf(" Free SIGINT uses: %d/5\n", ++counter);  // num of times ^c was used
@@ -57,9 +76,15 @@ void signalOptions(int signo) {
     }
 }
 
+// void exitProgram(int signo);
+// Parameters:
+//      int signo:  this paremater catches the signal that is given while in the main function
+// Description:
+//      When called, this function alerts the user that they have exceeded the maximum number
+//      of SIGINT uses and then resets the action of SIGINT and then terminates the program by 
+//      killing the program using SIGINT and the main program's process ID. 
 void exitprogram(int signo) {
     signal(SIGINT, SIG_DFL);  // returns ^c to default handler
-    printf(" Maximum SIGINT uses exceeded! Exiting program.\n");
-    done = 1;
-    // kill(getpid(), SIGINT);  // kills current process using ^c
+    printf(" Maximum SIGINT uses exceeded!(+5) Exiting program.\n");
+    kill(getpid(), SIGINT);  // kills current process using ^c
 }
